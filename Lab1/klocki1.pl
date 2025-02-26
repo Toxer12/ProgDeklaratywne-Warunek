9ef22d24389e3dbead4ac4c18102ed4473ca8c19
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2025-02-26 12:24:37 +0100

    Add files via upload

---
+++ Lab1/klocki1.pl
@@ -0,0 +1,24 @@
+% PROGRAM: klocki_1
+% Baza wiedzy o uk³adzie klocków
+% Definiowane predykaty:
+% na/2
+% ===
+
+
+% na(X,Y)
+% opis spe³niony, gdy klocek x le¿y
+% bezpoœrednio na klocku y
+% ---na/2
+na(c,a).
+na(c,b).
+na(d,c).
+% ---na/2
+
+/*
+ *Informacje o budowie programu:
+ *program sklada sie z 3 klauzul.
+ *program zawiera 1 definicje relacji.
+ *jewst to relacja na/2
+ *definicja relacji na/2 sklada sie z 3
+ *klauzul ktore sa faktami.
+ */
